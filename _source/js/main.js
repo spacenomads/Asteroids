729d3a57c9bf5f7dda8436e741733e2da7960c3b
Carlos Mañas <carlos@spacenomads.com>
2016-10-12 16:11:44 +0200

    - Update jquery to v3.1.1 - Update modernizr to v3.3.1

--- _source/js/main.js
+++ _source/js/main.js
@@ -1,18 +1,19 @@
 var COOKIES = {
-	ga_id : null,
-	ga_code : null,
-	ga_container : null,
+	gaID : null,
+	gaCode : null,
+	gaContainer : null,
 	lopd : false,
 
 	init : function() {
-		COOKIES.ga_id = 'UA-XXXXXXX-XX';
-		COOKIES.ga_code = "<script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');ga('create', '" + COOKIES.ga_id + "' , 'yoursite.com');ga('send', 'pageview');</script>";
-		COOKIES.ga_container = $('#cookie_alert');
+		COOKIES.gaID = 'UA-XXXXXXX-XX';
+		COOKIES.gaYourSite = 'yoursite.com';
+		COOKIES.gaCode = "<script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');ga('create', '" + COOKIES.gaID + "' , '" + COOKIES.gaYourSite + "');ga('send', 'pageview');</script>";
+		COOKIES.gaContainer = $('#cookie_alert');
 		//console.log('COOKIES (Y,N): ' + $.cookie('lopd'));
 		if ($.cookie('lopd')) {
 			COOKIES.accepted();
 		} else {
-			COOKIES.ga_container.addClass('shown').find('button').on('click', function() {
+			COOKIES.gaContainer.addClass('shown').find('button').on('click', function() {
 				if (!COOKIES.lopd) {
 					COOKIES.accepted();
 				}
@@ -23,20 +24,24 @@ var COOKIES = {
 	accepted : function() {
 		$.cookie('lopd', 'true');
 		COOKIES.lopd = true;
-		COOKIES.ga_container.removeClass('shown').html(COOKIES.ga_code);
+		COOKIES.gaContainer.removeClass('shown').html(COOKIES.gaCode);
 	}
 };
 
-jQuery(document).ready(function($) {
+$(function() {
+
+	const
+		$body = $('body'),
+		$externalLinks = $body.find('.legal a, a.ext');
 	// Responsive snitch
-	if ( $('body').hasClass('_dev') ) {
-		$('body').width_snitch({
+	if ( $body.hasClass('_dev') ) {
+		$body.width_snitch({
 			style:{}
 		})
 	}
 
 	// External links
-	$('.legal a, a.ext').attr('target', '_blank');
+	$externalLinks.attr('target', '_blank');
 
 	// EU Cookie law
 	COOKIES.init();
