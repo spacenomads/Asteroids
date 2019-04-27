14f14731eb36cf12b87a4635f5cd3fd7d6d2724a
Carlos Mañas <carlos@spacenomads.com>
2019-04-28 01:39:17 +0200

    Add dev functions

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,49 @@
-var autoprefixer = require('gulp-autoprefixer');
-var browserSync  = require('browser-sync');
-var cache        = require('gulp-cached');
-var combineMq    = require('gulp-combine-mq');
-var concat       = require('gulp-concat');
-var config       = require('./config.json');
-var cssminifiy   = require('gulp-clean-css');
-var del          = require('del');
-var gulp         = require('gulp');
-var gutil        = require('gulp-util');
-var notify       = require('gulp-notify');
-var plumber      = require('gulp-plumber');
-var pug          = require('gulp-pug');
-var reload       = browserSync.reload;
-var rename       = require('gulp-rename');
-var runSequence  = require('run-sequence');
-var sass         = require('gulp-sass');
-var sourcemaps   = require('gulp-sourcemaps');
-var uglify       = require('gulp-uglify');
-var zip          = require('gulp-zip');
+const { src, dest, series, parallel, watch } = require('gulp');
+const autoprefixer = require('gulp-autoprefixer');
+const browserSync = require('browser-sync').create();
+const cache = require('gulp-cached');
+const combineMq = require('gulp-combine-mq');
+const concat = require('gulp-concat');
+const config = require('./config.json');
+const cssminifiy = require('gulp-clean-css');
+const del = require('del');
+const gutil = require('gulp-util');
+const notify = require('gulp-notify');
+const plumber = require('gulp-plumber');
+const pug = require('gulp-pug');
+const reload = browserSync.reload;
+const rename = require('gulp-rename');
+const runSequence  = require('run-sequence');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+const uglify = require('gulp-uglify');
+const zip = require('gulp-zip');
 
 
 
 
 
 // > Manage task's errors
-var onError = function (err) {
-	gutil.beep();
-	console.log(err);
+const onError = (err) => console.log(err);
+
+
+
+
+
+// > Force a browser page reload
+const bsReload = cb => {
+	browserSync.reload();
+	cb();
+};
+
+
+
+
+
+// > Delete Public folder
+const clean = cb => {
+	del.sync(['public']);
+	cb();
 };
 
 
@@ -34,83 +51,63 @@ var onError = function (err) {
 
 
 // > Copy Icons
-gulp.task('icons', function () {
-	return gulp.src(config.icons.src)
-		.pipe(gulp.dest(config.icons.dest))
-		.pipe(notify({message: '> Icons OK', onLast: true}));
-});
+const icons = () => {
+	return src(config.icons.src)
+		.pipe(dest(config.icons.dest));
+};
 
 
 
 
 
 // > Copy Images
-gulp.task('images', function () {
-	return gulp.src(config.images.src)
-		.pipe(gulp.dest(config.images.dest))
-		.pipe(notify({message: '> Images OK', onLast: true}));
-});
+const images = () => {
+	return src(config.images.src)
+		.pipe(dest(config.images.dest));
+};
 
 
 
 
 
 // > Copy Vendor JS (Jquery, Modernizr..)
-gulp.task('vendor-js', function () {
-	return gulp.src(config.vendorJS.src)
-		.pipe(gulp.dest(config.vendorJS.dest))
-		.pipe(notify({message: '> Vendor JS OK', onLast: true}));
-});
+const vendorJS = () => {
+	return src(config.vendorJS.src)
+		.pipe(dest(config.vendorJS.dest));
+};
 
 
 
 
 
 // > Copy humansTXT
-gulp.task('humansTXT', function () {
-	return gulp.src(config.humansTXT.src)
-		.pipe(gulp.dest(config.humansTXT.dest))
-		.pipe(notify({message: '> HumansTXT OK', onLast: true}));
-});
+const humansTXT = () => {
+	return src(config.humansTXT.src)
+		.pipe(dest(config.humansTXT.dest));
+};
 
 
 
 
 
 // > Process .PUG files into 'public' folder
-gulp.task( 'templates' , function(cb) {
-	return gulp.src(config.templates.src)
+const templates = () => {
+	return src(config.templates.src)
 		.pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
 		.pipe(cache('templatesCache'))
 		.pipe(pug({
 			pretty: '\t'
 		}))
-		.pipe(gulp.dest(config.templates.dest))
-		.pipe(notify({message: '> Templates OK', onLast: true}));
-});
-
-
-
-
-
-// > Process partials .Pug files into 'public' folder
-gulp.task( 'templatePartials' , function(cb) {
-	return gulp.src(config.templates.src)
-		.pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
-		.pipe(pug({
-			pretty: '\t'
-		}))
-		.pipe(gulp.dest(config.templates.dest))
-		.pipe(notify({message: '> Complete templates OK', onLast: true}));
-});
+		.pipe(dest(config.templates.dest));
+};
 
 
 
 
 
 // > Process SASS/SCSS files to generate final css files in 'public' folder
-gulp.task( 'styles' , function(cb) {
-	return gulp.src(config.styles.src)
+const styles = () => {
+	return src(config.styles.src)
 		.pipe(sourcemaps.init())
 		.pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
 		.pipe(sass({
@@ -127,15 +124,50 @@ gulp.task( 'styles' , function(cb) {
 			cascade: false
 		}))
 		.pipe(sourcemaps.write('./'))
-		.pipe(gulp.dest(config.styles.dest))
-		.pipe(browserSync.reload({ stream:true }))
-		.pipe(notify({message: '> CSS OK', onLast: true}));
-});
+		.pipe(dest(config.styles.dest))
+		.pipe(browserSync.stream());
+};
 
 
 
 
 
+// > Process JS scripts into a single JS file inside 'assets/js' folder
+const scripts= () => {
+	return src(config.scripts.src)
+		.pipe(sourcemaps.init())
+		.pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
+		.pipe(concat('main.min.js'))
+		.pipe(sourcemaps.write('./'))
+		.pipe(dest(config.scripts.dest));
+};
+
+
+
+
+
+// > Process plugins into a single JS file inside 'assets/js' folder
+const plugins = () => {
+	return src(config.plugins.src)
+		.pipe(sourcemaps.init())
+		.pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
+		.pipe(concat('plugins.js'))
+		.pipe(sourcemaps.write('./'))
+		.pipe(dest(config.plugins.dest));
+};
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 // > Process SASS/SCSS files to generate final css files in 'public' folder
 gulp.task( 'styles-min' , function(cb) {
 	return gulp.src(config.styles.src)
@@ -161,21 +193,6 @@ gulp.task( 'styles-min' , function(cb) {
 
 
 
-// > Process plugins into a single JS file inside 'assets/js' folder
-gulp.task('plugins', function(){
-	return gulp.src(config.plugins.src)
-		.pipe(sourcemaps.init())
-		.pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
-		.pipe(concat('plugins.js'))
-		.pipe(sourcemaps.write('./'))
-		.pipe(gulp.dest(config.plugins.dest))
-		.pipe(browserSync.reload({ stream:true }))
-		.pipe(notify({message: 'PLUGINS OK', onLast: true}));
-});
-
-
-
-
 
 // > Process plugins into a single JS file inside 'assets/js' folder without sourcemaps
 gulp.task('plugins-clean', function(){
@@ -190,22 +207,6 @@ gulp.task('plugins-clean', function(){
 
 
 
-// > Process JS scripts into a single JS file inside 'assets/js' folder
-gulp.task('scripts', function(){
-	return gulp.src(config.scripts.src)
-		.pipe(sourcemaps.init())
-		.pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
-		.pipe(concat('main.js'))
-		//.pipe(uglify())
-		.pipe(sourcemaps.write('./'))
-		.pipe(gulp.dest(config.scripts.dest))
-		.pipe(browserSync.reload({ stream:true }))
-		.pipe(notify({message: 'JS OK', onLast: true}));
-});
-
-
-
-
 
 // > Process JS scripts into a single minified JS file inside 'assets/js' folder
 gulp.task('scripts-min', function(){
@@ -215,37 +216,6 @@ gulp.task('scripts-min', function(){
 		.pipe(uglify())
 		.pipe(gulp.dest(config.scripts.dest))
 		.pipe(notify({message: 'JS MIN OK', onLast: true}));
-});
-
-
-
-
-
-// > Create a development server with BrowserSync
-gulp.task('go', ['default'], function () {
-	browserSync.init({
-		server : {
-			baseDir: "public"
-		},
-		ghostMode: false,
-		online: true
-	});
-	gulp.watch(config.watch.images, ['bs-reload', ['images']]);
-	gulp.watch(config.watch.vendorJS, ['bs-reload', ['vendor-js']]);
-	gulp.watch(config.watch.humansTXT, ['humansTXT']);
-	gulp.watch(config.watch.styles, ['styles']);
-	gulp.watch(config.watch.scripts, ['scripts', 'plugins']);
-	gulp.watch(config.watch.templates, ['bs-reload', ['templates']]);
-	gulp.watch(config.watch.templatePartials, ['bs-reload', ['templatePartials']]);
-});
-
-
-
-
-
-// > Force a browser page reload
-gulp.task('bs-reload', function () {
-	browserSync.reload();
 });
 
 
@@ -276,9 +246,46 @@ gulp.task('default', ['clean'], function (cb) {
 gulp.task('deploy', ['clean'], function (cb) {
 	runSequence('styles-min', ['icons', 'images', 'vendor-js', 'humansTXT', 'templates', 'templatePartials', 'plugins-clean', 'scripts-min'], cb);
 });
+*/
 
 
 
 
-// > Delete Public folder
-gulp.task('clean', del.bind(null, ['public']));
+
+// > Generate public folder
+const defaultTasks = series(clean, icons, images, humansTXT, vendorJS, templates, styles, scripts, plugins);
+
+
+
+
+
+// > Create a development server with BrowserSync
+const go = series(defaultTasks, cb => {
+	browserSync.init({
+		server: {
+			baseDir: "public"
+		},
+		online: false
+	});
+	watch(config.watch.images, series(images, bsReload));
+	watch(config.watch.vendorJS, series(vendorJS, bsReload));
+	watch(config.watch.humansTXT, humansTXT);
+	watch(config.watch.styles, styles);
+	watch(config.watch.scripts, series(plugins, scripts, bsReload));
+	watch(config.watch.templates, series(templates, bsReload));
+	cb();
+});
+
+module.exports = { 
+	clean, 
+	icons, 
+	images, 
+	humansTXT, 
+	vendorJS, 
+	templates, 
+	styles, 
+	scripts,
+	plugins,
+	go
+};
+module.exports.default = defaultTasks;
